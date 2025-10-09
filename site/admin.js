9f0562084290284a11fa161c4e1973da9f50f170
kerpat2003 <yakerpat@mail.ru>
2025-10-10 04:32:49 +0500

    refactor: использовать URL из конфига вместо захардкоженного

--- site/admin.js
+++ site/admin.js
@@ -2334,7 +2334,7 @@ clientsTableBody.addEventListener('click', async (e) => {
                 if (rentalFetchError) throw new Error('Не удалось получить данные аренды для отправки акта.');
                 const userId = rental.user_id;
 
-                const pdfServerUrl = 'https://dogovorbikepark54.onrender.com/api/user';
+                const pdfServerUrl = `${window.APP_CONFIG.CONTRACTS_API_URL}/api/user`;
                 const pdfResponse = await fetch(pdfServerUrl, {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
